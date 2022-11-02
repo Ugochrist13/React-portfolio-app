f9893ceec9c2f7809a2ba814c98ec50d0c7196b8
Christian <christianugoo@gmail.com>
2022-11-02 03:59:56 +0100

    push up

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,21 +1,42 @@
-import { Box, Flex, Heading, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Icon,
+  Text,
+  useColorMode,
+  useMediaQuery,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 
 function Profile() {
-
   const { colorMode } = useColorMode();
   const isDark = colorMode === "dark";
   const { isLS } = useMediaQuery("(min-width:650px)");
-  
+
   return (
     <VStack className="profile" w="100%">
-      <Flex w="50%" justify="space-between">
-        <VStack >
+      <Flex w="70%" justify="space-between">
+        <VStack justify="center" spacing="15px" >
           <Heading>2+</Heading>
           <Text>Years of Experience</Text>
         </VStack>
-        <VStack><Text>Front End Developer, specialized on websites and web application using React</Text>
-        <Box w="300px" h="400px" bg={isDark? "purple.400": "gray.500"} ></Box></VStack>
+        <VStack w="80%" >
+          <Text>
+            Front End Developer, specialized on websites and web application
+            using React
+          </Text>
+          <Flex flexWrap="wrap" gap="20px">
+            <Box
+              w="200px"
+              h="200px"
+              bg={isDark ? "gray.300" : "purple.700"}
+            >
+              <Icon as={TbBrandJavascript}/><Text>C</Text>
+            </Box>
+          </Flex>
+        </VStack>
       </Flex>
     </VStack>
   );
